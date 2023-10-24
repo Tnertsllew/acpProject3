65c3bc6624dd529d1dfc69042c0dcaf68ef8c93a
Trent Wells <115761015+Tnertsllew@users.noreply.github.com>
2023-10-24 11:59:39 -0500

    Add files via upload

--- App.java
+++ App.java
@@ -1,29 +1,111 @@
 package uwf.fxproject;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Line;
+
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 /**
  * JavaFX App
  */
+
+//critical section is who wins the race
 public class App extends Application {
 	
-	private Gui gui;
+	private Horse[] horses;
+	private BorderPane root;
+	Lock l = new ReentrantLock();
 
     @Override
     public void start(Stage stage) {
-    	 gui = new Gui();
-         stage.setScene(gui.buildScene());
+         stage.setScene(buildScene());
          stage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
+    
+    public void runRace() {
+    	//loop to start the horses
+    	for(int i = 0; i < 6; i++) {
+    		horses[i].startHorseThread();
+    	}
+    }
+    
+    
+    private Scene buildScene() {
+		horses = new Horse[6];
+		for(int i = 0; i < 6; i++) {
+			horses[i] = new Horse(this, i);
+		}
+		root = new BorderPane();
+		HBox buttonBox = new HBox(10);
+		buttonBox.getChildren().addAll(createRunButton(), createResetButton(), createQuitButton());
+		root.setBottom(buttonBox);
+		VBox horseBox = new VBox();
+		for(int i = 0; i < 6; i++) {
+			horseBox.getChildren().add(horses[i].getCanvas());
+		}
+		root.getChildren().add(horseBox);
+		root.getChildren().add(createFinishLine());
+		Scene scene = new Scene(root, 800, 500);
+		return scene;
+	}
+    
+    public void stopAll(int winner)
+    {
+      for(int i = 0; i < 6; i++)
+      {
+    	  if(i != winner) {
+    		  horses[i].stopHorse();
+    	  }
+      }
+    }
+    
+    private Line createFinishLine() {
+        Line finishLine = new Line();
+        finishLine.setStartX(700);
+        finishLine.setStartY(0);
+        finishLine.setEndX(700);
+        finishLine.setEndY(500);
+        return finishLine;
+    }
+	
+	private Button createQuitButton() {
+		Button quit = new Button("Quit");
+		quit.setOnAction(event -> {
+			System.exit(0);
+        });
+		return quit;
+	}
+	
+	private Button createResetButton() {
+		Button reset = new Button("Reset");
+		reset.setOnAction(event -> {
+		     for(int i = 0; i < 6 ; i++) {
+		            horses[i].stopHorse();
+		            horses[i].getCanvas().setTranslateX(0);  // reset position
+		        }
+        });
+		return reset;
+	}
+
+	private Button createRunButton() {
+		Button run = new Button("Run");
+		run.setOnAction(event -> {
+			runRace();
+        });
+		return run;
+	}
 
 }
--- Horse.java
+++ Horse.java
@@ -3,24 +3,31 @@ package uwf.fxproject;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import java.util.Random;
 
 public class Horse implements Runnable{
 	private Canvas c;
+	private Thread t;
+	private boolean hasWon = false;
+	private App app;
+	private int horseNum;
 	
-	public Horse() {
-		draw();
+	public Horse(App app, int horseNum) {
+		 drawHorse();
+		 this.app = app;
+		 this.horseNum = horseNum;
 	}
 
-	private void draw() {
+	private void drawHorse() {
 		c = new Canvas(80, 80);
 		GraphicsContext gc = c.getGraphicsContext2D();
 		
 		// Body
 		gc.setFill(Color.BLACK);
-		gc.fillOval(15, 15, 40, 20);
+		gc.fillRect(15, 15, 40, 20);
 
 		// Head
-		gc.fillOval(50, 10, 15, 17);
+		gc.fillRect(50, 10, 15, 17);
 
 		// Legs
 		gc.fillRect(20, 32, 3, 15);  // Front left leg
@@ -36,8 +43,47 @@ public class Horse implements Runnable{
 		return c;
 	}
 	
+	public boolean getHasWon() {
+		return hasWon;
+	}
+	
+	public void startHorseThread() {
+		t = new Thread(this);
+		t.start();
+	}
+
+	public void stopHorse() {
+		t.interrupt();
+	}
+	
+	
 	@Override
     public void run() {
-        
+		   Random rand = new Random();
+		    while (true) {  // Continuously move the horse until interrupted
+		    	app.l.lock();
+		        double randomX = 5 + rand.nextInt(30);
+		        double currentX = c.getTranslateX();
+		        c.setTranslateX(currentX + randomX);
+
+		        System.out.println(currentX);
+		        try {
+		            if (currentX > 620 && !hasWon) {
+		                hasWon = true;
+		                app.stopAll(horseNum);
+		                System.out.println("Horse has won!" + horseNum);
+		                break;
+		            }
+		        } finally {
+		            app.l.unlock();
+		        }
+		        try {
+		            Thread.sleep(50);
+		        } catch (InterruptedException e) {
+		        	System.out.println("thread interrupted");
+		            break; // If the thread is interrupted, exit the loop
+		            
+		        }
+		    }
     }
 }
